6396a4e459b1b440dd96e69f33dfc073a17ab3cc
howardcameron2 <cwhowar3@asu.edu>
2017-02-14 15:18:40 -0700

    cleaning up some confusion

--- PHY494-assignments-skeleton-master/assignment_02/silly2.py
+++
@@ -1,9 +0,0 @@
-# now to turn this into a function...
-def translate(coordinates, t):
-    for n in range(len(coordinates)):
-        coordinates[n]=[coordinates[n][0] +t[0],coordinates[n][1] +t[1],coordinates[n][2] +t[2]]
-
-coordinates = [[0, 0, 0], [1, 1, 1], [2, 2, 2], [3, 3, 3]]
-t = [1, 1, 1]
-translate(coordinates, t)
-print(coordinates)
